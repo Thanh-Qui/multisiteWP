b41b10319b798bbd863380b2c5e81f60e6955e8b
Thanh-Qui <quiluong111@gmail.com>
2025-12-01 17:53:15 +0700

    Create multisite (2 site) and setup woocommerce for main site

---
+++ wp-config.php
@@ -0,0 +1,124 @@
+<?php
+/**
+ * The base configuration for WordPress
+ *
+ * The wp-config.php creation script uses this file during the installation.
+ * You don't have to use the website, you can copy this file to "wp-config.php"
+ * and fill in the values.
+ *
+ * This file contains the following configurations:
+ *
+ * * Database settings
+ * * Secret keys
+ * * Database table prefix
+ * * ABSPATH
+ *
+ * @link https://developer.wordpress.org/advanced-administration/wordpress/wp-config/
+ *
+ * @package WordPress
+ */
+
+/** @desc this loads the composer autoload file */
+require_once __DIR__ . '/vendor/autoload.php';
+/** @desc this instantiates Dotenv and passes in our path to .env */
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+// ** Database settings - You can get this info from your web host ** //
+/** The name of the database for WordPress */
+define( 'DB_NAME', $_ENV['DB_NAME'] );
+
+/** Database username */
+define( 'DB_USER', $_ENV['DB_USER'] );
+
+/** Database password */
+define( 'DB_PASSWORD', $_ENV['DB_PASSWORD'] );
+
+/** Database hostname */
+define( 'DB_HOST', $_ENV['DB_HOST'] );
+
+/** Database charset to use in creating database tables. */
+define( 'DB_CHARSET', $_ENV['DB_CHARSET'] );
+
+/** The database collate type. Don't change this if in doubt. */
+define( 'DB_COLLATE', $_ENV['DB_COLLATE'] );
+
+if ( !defined('WP_CLI') ) {
+    define( 'WP_SITEURL', $_SERVER['REQUEST_SCHEME'] . '://' . $_SERVER['HTTP_HOST'] );
+    define( 'WP_HOME',    $_SERVER['REQUEST_SCHEME'] . '://' . $_SERVER['HTTP_HOST'] );
+}
+
+
+
+/**#@+
+ * Authentication unique keys and salts.
+ *
+ * Change these to different unique phrases! You can generate these using
+ * the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}.
+ *
+ * You can change these at any point in time to invalidate all existing cookies.
+ * This will force all users to have to log in again.
+ *
+ * @since 2.6.0
+ */
+define( 'AUTH_KEY',         $_ENV['AUTH_KEY'] );
+define( 'SECURE_AUTH_KEY',  $_ENV['SECURE_AUTH_KEY'] );
+define( 'LOGGED_IN_KEY',    $_ENV['LOGGED_IN_KEY'] );
+define( 'NONCE_KEY',        $_ENV['NONCE_KEY'] );
+define( 'AUTH_SALT',        $_ENV['AUTH_SALT'] );
+define( 'SECURE_AUTH_SALT', $_ENV['SECURE_AUTH_SALT'] );
+define( 'LOGGED_IN_SALT',   $_ENV['LOGGED_IN_SALT'] );
+define( 'NONCE_SALT',       $_ENV['NONCE_SALT'] );
+
+/**#@-*/
+
+/**
+ * WordPress database table prefix.
+ *
+ * You can have multiple installations in one database if you give each
+ * a unique prefix. Only numbers, letters, and underscores please!
+ *
+ * At the installation time, database tables are created with the specified prefix.
+ * Changing this value after WordPress is installed will make your site think
+ * it has not been installed.
+ *
+ * @link https://developer.wordpress.org/advanced-administration/wordpress/wp-config/#table-prefix
+ */
+$table_prefix = 'wp_';
+
+/**
+ * For developers: WordPress debugging mode.
+ *
+ * Change this to true to enable the display of notices during development.
+ * It is strongly recommended that plugin and theme developers use WP_DEBUG
+ * in their development environments.
+ *
+ * For information on other constants that can be used for debugging,
+ * visit the documentation.
+ *
+ * @link https://developer.wordpress.org/advanced-administration/debug/debug-wordpress/
+ */
+define('WP_DEBUG', $_ENV['WP_DEBUG']);
+define('WP_DEBUG_LOG', $_ENV['WP_DEBUG_LOG']);
+define('WP_DEBUG_DISPLAY', $_ENV['WP_DEBUG_DISPLAY']);
+define('SCRIPT_DEBUG', $_ENV['SCRIPT_DEBUG']);
+
+/* Add any custom values between this line and the "stop editing" line. */
+
+
+define("WP_ALLOW_MULTISITE", true);
+define( 'MULTISITE', true );
+define( 'SUBDOMAIN_INSTALL', true );
+define( 'DOMAIN_CURRENT_SITE', 'multisitewp.test:8080' );
+define( 'PATH_CURRENT_SITE', '/' );
+define( 'SITE_ID_CURRENT_SITE', 1 );
+define( 'BLOG_ID_CURRENT_SITE', 1 );
+/* That's all, stop editing! Happy publishing. */
+
+/** Absolute path to the WordPress directory. */
+if ( ! defined( 'ABSPATH' ) ) {
+	define( 'ABSPATH', __DIR__ . '/' );
+}
+
+/** Sets up WordPress vars and included files. */
+require_once ABSPATH . 'wp-settings.php';
